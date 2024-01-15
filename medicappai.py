960ea7b16b7ec34259ca73a11292ab3b8ed0773a
Antonin Rouxel <antonin.rouxel44@outlook.fr>
2024-01-15 22:05:07 +0100

    Killed history

---
+++ medicappai.py
@@ -0,0 +1,301 @@
+import pandas as pd
+import torch
+import numpy as np
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from torch.optim import SGD
+from transformers import CamembertForTokenClassification, CamembertTokenizerFast
+from torch.utils.mobile_optimizer import optimize_for_mobile
+import torch.backends._nnapi.prepare
+
+tokenizer = CamembertTokenizerFast.from_pretrained('camembert-base', torchscript=True)
+
+use_cuda = torch.cuda.is_available()
+
+df = pd.read_json('sentenceDatas.json')
+
+label_all_tokens = False
+
+def align_label(texts, labels):
+    tokenized_inputs = tokenizer(texts, padding='max_length', max_length=512, truncation=True)
+
+    word_ids = tokenized_inputs.word_ids()
+
+    previous_word_idx = None
+    label_ids = []
+
+    for word_idx in word_ids:
+
+        if word_idx is None:
+            label_ids.append(-100)
+
+        elif word_idx != previous_word_idx:
+            try:
+                label_ids.append(labels_to_ids[labels[word_idx]])
+            except:
+                label_ids.append(-100)
+        else:
+            try:
+                label_ids.append(labels_to_ids[labels[word_idx]] if label_all_tokens else -100)
+            except:
+                label_ids.append(-100)
+        previous_word_idx = word_idx
+
+    return label_ids
+
+class DataSequence(torch.utils.data.Dataset):
+
+    def __init__(self, df):
+
+        lb = [i for i in df['Labels'].values.tolist()]
+        txt = df['Sentence'].values.tolist()
+        self.texts = [tokenizer(str(i),
+                               padding='max_length', max_length = 512, truncation=True, return_tensors="pt") for i in txt]
+        self.labels = [align_label(i,j) for i,j in zip(txt, lb)]
+
+    def __len__(self):
+
+        return len(self.labels)
+
+    def get_batch_data(self, idx):
+
+        return self.texts[idx]
+
+    def get_batch_labels(self, idx):
+
+        return torch.LongTensor(self.labels[idx])
+
+    def __getitem__(self, idx):
+
+        batch_data = self.get_batch_data(idx)
+        batch_labels = self.get_batch_labels(idx)
+
+        return batch_data, batch_labels
+    
+labels = [i for i in df['Labels'].values.tolist()]
+unique_labels = set()
+
+for lb in labels:
+        [unique_labels.add(i) for i in lb if i not in unique_labels]
+labels_to_ids = {k: v for v, k in enumerate(unique_labels)}
+ids_to_labels = {v: k for v, k in enumerate(unique_labels)}
+
+df_train, df_val, df_test = np.split(df.sample(frac=1, random_state=42),
+                            [int(.8 * len(df)), int(.9 * len(df))])
+
+class CamembertModel(torch.nn.Module):
+
+    def __init__(self):
+
+        super(CamembertModel, self).__init__()
+
+        self.bert = CamembertForTokenClassification.from_pretrained('camembert-base', num_labels=len(unique_labels), torchscript=True)
+
+    def forward(self, input_id, mask, label=None):
+
+        output = self.bert(input_ids=input_id, attention_mask=mask, labels=label, return_dict=False)
+
+        return output
+
+def train_loop(model, df_train, df_val):
+
+    train_dataset = DataSequence(df_train)
+    val_dataset = DataSequence(df_val)
+
+    train_dataloader = DataLoader(train_dataset, num_workers=4, batch_size=BATCH_SIZE, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, num_workers=4, batch_size=BATCH_SIZE)
+
+    
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    optimizer = SGD(model.parameters(), lr=LEARNING_RATE)
+
+    if use_cuda:
+        model = model.cuda()
+
+    best_acc = 0
+    best_loss = 1000
+
+    for epoch_num in range(EPOCHS):
+
+        total_acc_train = 0
+        total_loss_train = 0
+
+        model.train()
+
+        for train_data, train_label in tqdm(train_dataloader):
+
+            train_label = train_label.to(device)
+            mask = train_data['attention_mask'].squeeze(1).to(device)
+            input_id = train_data['input_ids'].squeeze(1).to(device)
+
+            optimizer.zero_grad()
+            loss, logits = model(input_id, mask, train_label)
+
+            for i in range(logits.shape[0]):
+
+              logits_clean = logits[i][train_label[i] != -100]
+              label_clean = train_label[i][train_label[i] != -100]
+
+              predictions = logits_clean.argmax(dim=1)
+              acc = (predictions == label_clean).float().mean()
+              total_acc_train += acc
+              total_loss_train += loss.item()
+
+            loss.backward()
+            optimizer.step()
+
+        model.eval()
+
+        total_acc_val = 0
+        total_loss_val = 0
+
+        for val_data, val_label in val_dataloader:
+
+            val_label = val_label.to(device)
+            mask = val_data['attention_mask'].squeeze(1).to(device)
+            input_id = val_data['input_ids'].squeeze(1).to(device)
+
+            loss, logits = model(input_id, mask, val_label)
+
+            for i in range(logits.shape[0]):
+
+              logits_clean = logits[i][val_label[i] != -100]
+              label_clean = val_label[i][val_label[i] != -100]
+
+              predictions = logits_clean.argmax(dim=1)
+              acc = (predictions == label_clean).float().mean()
+              total_acc_val += acc
+              total_loss_val += loss.item()
+
+        val_accuracy = total_acc_val / len(df_val)
+        val_loss = total_loss_val / len(df_val)
+
+        print(
+            f'Epochs: {epoch_num + 1} | Loss: {total_loss_train / len(df_train): .3f} | Accuracy: {total_acc_train / len(df_train): .3f} | Val_Loss: {total_loss_val / len(df_val): .3f} | Accuracy: {total_acc_val / len(df_val): .3f}')
+
+LEARNING_RATE = 5e-3
+EPOCHS = 10
+BATCH_SIZE = 2
+
+def evaluate(model, df_test):
+
+    test_dataset = DataSequence(df_test)
+
+    test_dataloader = DataLoader(test_dataset, num_workers=4, batch_size=1)
+
+    
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    if use_cuda:
+        model = model.cuda()
+
+    total_acc_test = 0.0
+
+    for test_data, test_label in test_dataloader:
+
+            test_label = test_label.to(device)
+            mask = test_data['attention_mask'].squeeze(1).to(device)
+
+            input_id = test_data['input_ids'].squeeze(1).to(device)
+
+            loss, logits = model(input_id, mask, test_label)
+
+            for i in range(logits.shape[0]):
+
+              logits_clean = logits[i][test_label[i] != -100]
+              label_clean = test_label[i][test_label[i] != -100]
+
+              predictions = logits_clean.argmax(dim=1)
+              acc = (predictions == label_clean).float().mean()
+              total_acc_test += acc
+
+    val_accuracy = total_acc_test / len(df_test)
+    print(f'Test Accuracy: {total_acc_test / len(df_test): .3f}')
+
+def align_word_ids(texts):
+  
+    tokenized_inputs = tokenizer(texts, padding='max_length', max_length=512, truncation=True)
+
+    word_ids = tokenized_inputs.word_ids()
+
+    previous_word_idx = None
+    label_ids = []
+
+    for word_idx in word_ids:
+
+        if word_idx is None:
+            label_ids.append(-100)
+
+        elif word_idx != previous_word_idx:
+            try:
+                label_ids.append(1)
+            except:
+                label_ids.append(-100)
+        else:
+            try:
+                label_ids.append(1 if label_all_tokens else -100)
+            except:
+                label_ids.append(-100)
+        previous_word_idx = word_idx
+
+    return label_ids
+
+
+def evaluate_one_text(model, sentence):
+
+    
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    if use_cuda:
+        model = model.cuda()
+
+    text = tokenizer(sentence, padding='max_length', max_length = 512, truncation=True, return_tensors="pt")
+
+    mask = text['attention_mask'].to(device)
+    input_id = text['input_ids'].to(device)
+    label_ids = torch.Tensor(align_word_ids(sentence)).unsqueeze(0).to(device)
+
+    logits = model(input_id, mask)
+    logits_clean = logits[0][label_ids != -100]
+
+    predictions = logits_clean.argmax(dim=1).tolist()
+    prediction_label = [ids_to_labels[i] for i in predictions]
+    return prediction_label
+
+if __name__ == '__main__':
+    #"""
+    df.head()
+    model = CamembertModel()
+    train_loop(model, df_train, df_val)
+    evaluate(model, df_test)
+    # Tracez le modèle
+    
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    sentence = "Doliprane 1000 mg, 1 fois par jour pendant 3 jours"
+
+    text = tokenizer(sentence, padding='max_length', max_length = 512, truncation=True, return_tensors="pt")
+
+    mask = text['attention_mask'].to(device)
+    input_id = text['input_ids'].to(device)
+
+    traced_script_module = torch.jit.trace(model, (input_id, mask))
+
+    if use_cuda:
+        traced_script_module.save("model_gpu.pt")
+
+        model_cpu = torch.jit.load("model_gpu.pt", map_location=torch.device('cpu'))
+
+        mask_cpu = text['attention_mask'].to(torch.device('cpu'))
+        input_id_cpu = text['input_ids'].to(torch.device('cpu'))
+
+        traced_script_module_cpu = torch.jit.trace(model_cpu, (input_id_cpu, mask_cpu))
+        traced_script_module_cpu.save("model_cpu.pt")
+    else:
+        traced_script_module.save("model_cpu.pt")
+
+    # Save id_to_label in txt file
+    with open('id_to_label.txt', 'w') as f:
+        for key, value in ids_to_labels.items():
+            f.write("%s\n" % value)
